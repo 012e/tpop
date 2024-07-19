02a1027685075af91b9653d6902f0ef4be00467e
012e <huyphmnhat@gmail.com>
2024-07-19 22:05:19 +0700

    add rustfmt + `hard_tabs=true` + format all files

--- src/session/mod.rs
+++ src/session/mod.rs
@@ -4,134 +4,134 @@ use std::process::Command;
 use color_eyre::eyre::Result;
 
 use crate::tmux::{
-    self,
-    popup::{new_popup, NewPopupConfig},
+	self,
+	popup::{new_popup, NewPopupConfig},
 };
 
 pub struct NewCommandPaneConfig {
-    pub command: String,
-    pub path: String,
-    pub silent: bool,
-    pub name: Option<String>,
+	pub command: String,
+	pub path: String,
+	pub silent: bool,
+	pub name: Option<String>,
 }
 
 #[derive(thiserror::Error, Debug)]
 pub enum PopupEror {
-    #[error("Popup already exists")]
-    AlreadyExists,
-    #[error("Popup does not exist")]
-    DoesNotExist,
-    #[error("Currently in normal mode, can't hide anything")]
-    CantHideNormalMode,
-    #[error("Currently in popup mode, can't show anything")]
-    CantShowPopup,
+	#[error("Popup already exists")]
+	AlreadyExists,
+	#[error("Popup does not exist")]
+	DoesNotExist,
+	#[error("Currently in normal mode, can't hide anything")]
+	CantHideNormalMode,
+	#[error("Currently in popup mode, can't show anything")]
+	CantShowPopup,
 }
 
 pub struct Session {
-    pub state: state::State,
-    pub name: String,
-    pub current_path: String,
+	pub state: state::State,
+	pub name: String,
+	pub current_path: String,
 }
 
 fn wrap_command(command: String) -> String {
-    format!("sh -c '{}; read'", command)
+	format!("sh -c '{}; read'", command)
 }
 
 impl Session {
-    pub fn current() -> Result<Self> {
-        let state = state::get_state()?;
-        let name = tmux::commands::get_session_name()?;
-        let current_path = tmux::commands::get_current_session_property("#{pane_current_path}")?;
-        Ok(Self {
-            state,
-            name,
-            current_path,
-        })
-    }
+	pub fn current() -> Result<Self> {
+		let state = state::get_state()?;
+		let name = tmux::commands::get_session_name()?;
+		let current_path = tmux::commands::get_current_session_property("#{pane_current_path}")?;
+		Ok(Self {
+			state,
+			name,
+			current_path,
+		})
+	}
 
-    fn get_popup_session_name(&self) -> String {
-        if self.name.starts_with("popup") {
-            self.name.clone()
-        } else {
-            format!("popup{}", self.name)
-        }
-    }
+	fn get_popup_session_name(&self) -> String {
+		if self.name.starts_with("popup") {
+			self.name.clone()
+		} else {
+			format!("popup{}", self.name)
+		}
+	}
 
-    pub fn toggle_popup(&self) -> Result<()> {
-        match self.state {
-            state::State::Normal => self.show_popup()?,
-            state::State::Popup => self.hide_popup()?,
-        }
-        Ok(())
-    }
+	pub fn toggle_popup(&self) -> Result<()> {
+		match self.state {
+			state::State::Normal => self.show_popup()?,
+			state::State::Popup => self.hide_popup()?,
+		}
+		Ok(())
+	}
 
-    pub fn hide_popup(&self) -> Result<()> {
-        if self.state == state::State::Popup {
-            Command::new("tmux").arg("detach").spawn()?;
-        }
-        Ok(())
-    }
+	pub fn hide_popup(&self) -> Result<()> {
+		if self.state == state::State::Popup {
+			Command::new("tmux").arg("detach").spawn()?;
+		}
+		Ok(())
+	}
 
-    pub fn show_popup(&self) -> Result<()> {
-        if let state::State::Normal = self.state {
-            self.create_popup_session()?;
-            new_popup(NewPopupConfig {
-                command: Some("tmux attach -t ".to_owned() + &self.get_popup_session_name()),
-                path: self.current_path.clone(),
-                height: Some(80),
-                width: Some(80),
-            })?;
-        }
-        Ok(())
-    }
+	pub fn show_popup(&self) -> Result<()> {
+		if let state::State::Normal = self.state {
+			self.create_popup_session()?;
+			new_popup(NewPopupConfig {
+				command: Some("tmux attach -t ".to_owned() + &self.get_popup_session_name()),
+				path: self.current_path.clone(),
+				height: Some(80),
+				width: Some(80),
+			})?;
+		}
+		Ok(())
+	}
 
-    pub fn add_popup_pane(&self) -> Result<()> {
-        Ok(())
-    }
+	pub fn add_popup_pane(&self) -> Result<()> {
+		Ok(())
+	}
 
-    pub fn spawn_command_window(
-        &self,
-        NewCommandPaneConfig {
-            command,
-            path,
-            silent,
-            name,
-        }: NewCommandPaneConfig,
-    ) -> Result<()> {
-        self.create_popup_session()?;
-        let mut cmd = Command::new("tmux");
-        cmd.arg("new-window")
-            .arg("-t")
-            .arg(&self.get_popup_session_name())
-            .arg("-n")
-            .arg(name.unwrap_or("cmdRunner".to_owned()))
-            .arg(command)
-            .spawn()?;
+	pub fn spawn_command_window(
+		&self,
+		NewCommandPaneConfig {
+			command,
+			path,
+			silent,
+			name,
+		}: NewCommandPaneConfig,
+	) -> Result<()> {
+		self.create_popup_session()?;
+		let mut cmd = Command::new("tmux");
+		cmd.arg("new-window")
+			.arg("-t")
+			.arg(&self.get_popup_session_name())
+			.arg("-n")
+			.arg(name.unwrap_or("cmdRunner".to_owned()))
+			.arg(command)
+			.spawn()?;
 
-        if !silent {
-            Command::new("tmux")
-                .arg("set-hook")
-                .arg("-t")
-                .arg(&self.get_popup_session_name())
-                .arg("pane-exited")
-                .arg("detach")
-                .spawn()?;
-            self.show_popup()?;
-        }
+		if !silent {
+			Command::new("tmux")
+				.arg("set-hook")
+				.arg("-t")
+				.arg(&self.get_popup_session_name())
+				.arg("pane-exited")
+				.arg("detach")
+				.spawn()?;
+			self.show_popup()?;
+		}
 
-        Ok(())
-    }
+		Ok(())
+	}
 
-    pub fn create_popup_session(&self) -> Result<()> {
-        let popup_session_name = format!("popup{}", self.name);
-        Command::new("tmux")
-            .arg("new-session")
-            .arg("-d")
-            .arg("-s")
-            .arg(popup_session_name)
-            .arg("-c")
-            .arg(self.current_path.clone())
-            .spawn()?;
-        Ok(())
-    }
+	pub fn create_popup_session(&self) -> Result<()> {
+		let popup_session_name = format!("popup{}", self.name);
+		Command::new("tmux")
+			.arg("new-session")
+			.arg("-d")
+			.arg("-s")
+			.arg(popup_session_name)
+			.arg("-c")
+			.arg(self.current_path.clone())
+			.spawn()?;
+		Ok(())
+	}
 }
--- src/tmux/commands.rs
+++ src/tmux/commands.rs
@@ -1,56 +1,56 @@
 use std::{io, process::Command};
 
 pub fn new_session_detached(name: &str, create_new: bool) -> Result<(), io::Error> {
-    let mut cmd = Command::new("tmux");
-    cmd.arg("new-session").arg("-d").arg("-s").arg(name);
-    if !create_new {
-        cmd.arg("-A");
-    }
-    cmd.spawn()?;
-    Ok(())
+	let mut cmd = Command::new("tmux");
+	cmd.arg("new-session").arg("-d").arg("-s").arg(name);
+	if !create_new {
+		cmd.arg("-A");
+	}
+	cmd.spawn()?;
+	Ok(())
 }
 
 pub fn new_session(name: &str, create_new: bool) -> Result<(), io::Error> {
-    let mut cmd = Command::new("tmux");
-    cmd.arg("new-session").arg("-s").arg(name);
-    if !create_new {
-        cmd.arg("-A");
-    }
-    cmd.spawn()?;
-    Ok(())
+	let mut cmd = Command::new("tmux");
+	cmd.arg("new-session").arg("-s").arg(name);
+	if !create_new {
+		cmd.arg("-A");
+	}
+	cmd.spawn()?;
+	Ok(())
 }
 
 pub fn run_command(command: &str, target: &str) -> Result<(), io::Error> {
-    Command::new("tmux")
-        .arg("send-keys")
-        .arg("-t")
-        .arg(target)
-        .arg(command)
-        .arg("Enter")
-        .spawn()?;
-    Ok(())
+	Command::new("tmux")
+		.arg("send-keys")
+		.arg("-t")
+		.arg(target)
+		.arg(command)
+		.arg("Enter")
+		.spawn()?;
+	Ok(())
 }
 
 pub fn get_current_session_property(property: &str) -> Result<String, io::Error> {
-    let output = Command::new("tmux")
-        .arg("display-message")
-        .arg("-p")
-        .arg(property)
-        .output()?;
-    Ok(String::from_utf8(output.stdout).unwrap().trim().into())
+	let output = Command::new("tmux")
+		.arg("display-message")
+		.arg("-p")
+		.arg(property)
+		.output()?;
+	Ok(String::from_utf8(output.stdout).unwrap().trim().into())
 }
 
 pub fn get_session_property(property: &str, target: &str) -> Result<String, io::Error> {
-    let output = Command::new("tmux")
-        .arg("display-message")
-        .arg("-p")
-        .arg(property)
-        .arg("-t")
-        .arg(target)
-        .output()?;
-    Ok(String::from_utf8(output.stdout).unwrap())
+	let output = Command::new("tmux")
+		.arg("display-message")
+		.arg("-p")
+		.arg(property)
+		.arg("-t")
+		.arg(target)
+		.output()?;
+	Ok(String::from_utf8(output.stdout).unwrap())
 }
 
 pub fn get_session_name() -> Result<String, io::Error> {
-    get_current_session_property("#S")
+	get_current_session_property("#S")
 }
